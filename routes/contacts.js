4b16c26ed1016a05f61eff5e679c76c0bf74d506
Keith Mair <mairck@gmail.com>
2023-01-23 20:38:11 -0700

    first commit

---
+++ routes/contacts.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contacts');
+
+router.get('/', contactsController.returnAllContacts);
+
+router.get('/:id', contactsController.findContactById);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+
+module.exports = router;
